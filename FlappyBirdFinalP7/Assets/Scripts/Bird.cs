9d548edd0e66238812315c7d98023a60fc6eabc5
DrewAzurinP7 <740723@student.dusd.net>
2022-12-21 12:29:37 -0800

    Added SFX to FlappyBird
    Added SFX to FlappyBird

--- FlappyBirdFinalP7/Assets/Scripts/Bird.cs
+++ FlappyBirdFinalP7/Assets/Scripts/Bird.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    AudioSource audioSource;
+
     public float upForce = 200f;
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
     private Animator anim;
+    public AudioClip flap;
+    public AudioClip die;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,6 +34,7 @@ public class Bird : MonoBehaviour
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
                 anim.SetTrigger("Flap");
+                PlaySound(flap);
             }
         }
     }
@@ -37,5 +45,11 @@ public class Bird : MonoBehaviour
         isDead = true;
         anim.SetTrigger("Die");
         GameControl.instance.BirdDied ();
+        PlaySound(die);
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
--- FlappyBirdFinalP7/Assets/Scripts/Column.cs
+++ FlappyBirdFinalP7/Assets/Scripts/Column.cs
@@ -1,14 +1,19 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Column : MonoBehaviour
 {
+    public AudioClip score;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Bird controller = other.GetComponent<Bird>();
+
         if (other.GetComponent<Bird>() != null)
         {
             GameControl.instance.BirdScored();
+            controller.PlaySound(score);
         }
     }
 }
